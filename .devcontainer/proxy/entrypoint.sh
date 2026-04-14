2d8af34aac711b6caa2f85ee185565ae7039ac5b
Jarkko Kallio <kallja@gmail.com>
2026-04-14 23:28:51 +0300

    chore: changes again

--- .devcontainer/proxy/entrypoint.sh
+++ .devcontainer/proxy/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 set -e
 
+# ssl_verify_upstream_trusted_ca=... enables trust for Cloudflare WARP certificate.
 exec mitmweb --web-host 0.0.0.0 --listen-port 8080 \
-  --set confdir=/home/mitmproxy/.mitmproxy \
-  # This enables trust for Cloudflare WARP certificate. Host certs are available in containers.
   --set ssl_verify_upstream_trusted_ca=/etc/ssl/certs/ca-certificates.crt \
-  --no-web-open-browser \
-  --set web_password='$argon2i$v=19$m=8,t=1,p=1$YWFhYWFhYWE$nXD9kg' \
+  --set web_password="$PASSWORD_HASH" \
   --set termlog_verbosity=info \
-  -s /addon.py
+  -s /addons/addon.py
